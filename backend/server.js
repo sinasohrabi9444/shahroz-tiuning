2e2c2bd86ee047dc5c259033ef541f4ddbe0c8ed
sina <sinasohrabi9444@gmail.com>
2024-09-08 16:15:00 +0330

    add .env

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // اتصال به MongoDB
-mongoose.connect('mongodb://localhost:27017/carhub', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
